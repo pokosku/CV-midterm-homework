75009f56b8c7c36f688598eef14deed504e0515a
Dercombi <daniele.riolmirossetto@gmail.com>
2026-04-22 13:10:29 +0200

    data loader con gestione errori, non eccezione, ma ritorna una mat vuota

--- include/DataLoader.hpp
+++ include/DataLoader.hpp
@@ -3,7 +3,9 @@
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
+#include <iostream>
 using namespace cv;
 using namespace std;
+
 Mat LoadImg(string folder, int number);
 #endif
--- src/DataLoader.cpp
+++ src/DataLoader.cpp
@@ -1,11 +1,31 @@
 #include "DataLoader.hpp"
 
-Mat LoadImg(string folder,int number){
     const string PATHIMAGES="../assets/data/";
     const string BIRD="bird";
     const string CAR="car";
     const string FROG="frog";
     const string SHEEP="sheep";
     const string SQUIRREL="squirrel";
-    return imread(PATHIMAGES+folder+"/000"+to_string(number)+".png");
+
+Mat LoadImg(string folder,int number){
+    string numberString=to_string(number);
+    string zeros="/";
+    Mat img;
+    if(number>278){
+        cout<<"Error number"<<endl;
+        return Mat{};
+    }
+    for(int i=0;i<4-numberString.length();i++){
+        zeros=zeros+"0";
+    }
+    if(folder!=CAR && folder!=BIRD && folder!=FROG && folder!=SHEEP && folder!=SQUIRREL){
+        cout<<"Error folder"<<endl;
+        return Mat{};
+    }
+    img=imread(PATHIMAGES+folder+zeros+numberString+".png");
+    if(img.empty()){
+        return imread(PATHIMAGES+folder+zeros+numberString+".jpg");
+    }else{
+        return img;
+    }
 }
