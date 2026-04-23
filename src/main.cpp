2aa1df396f086862af177de1de6144e647769866
pokosku <fabbroleonardo@gmail.com>
2026-04-23 14:39:58 +0200

    test main

--- src/main.cpp
+++ src/main.cpp
@@ -13,33 +13,32 @@ int main(int argc, char** argv){
     
     
     char pressed_key = 'q';
-    cv::Mat img1 = dl.load_next_img(10);
-    cv::Mat test = img1.clone();
-
+    cv::Mat frame_0 = dl.load_next_img();
+    //cv::Mat test = img1.clone();
+    std::vector<cv::KeyPoint> kp0;
+    cv::Mat descriptor0;
+    detect_features(frame_0, kp0, descriptor0);
     cv::namedWindow("test");
     
     while(pressed_key != 'x'){
-        std::vector<cv::KeyPoint> kp1,kp2;
-        cv::Mat descriptor1, descriptor2;
-        cv::Mat img2 = dl.load_next_img(10);
+        std::vector<cv::KeyPoint> kp_t;
+        cv::Mat descriptor_t;
+        cv::Mat frame_t = dl.load_next_img();
 
-        detect_features(img1, kp1, descriptor1);
-        detect_features(img2, kp2, descriptor2);
+        
+        detect_features(frame_t, kp_t, descriptor_t);
 
-        std::vector<cv::Point2f> points = match_features(descriptor1, kp1, descriptor2, kp2);
+        std::vector<cv::Point2f> points = match_features(descriptor0, kp0, descriptor_t, kp_t);
         std::vector<cv::KeyPoint> keypoints;
 
         for(auto &p : points){
             keypoints.push_back(cv::KeyPoint(p, 1.0));
         }
         
-        cv::Mat output = test.clone();
-        cv::drawKeypoints(test, keypoints, output);
+        cv::Mat output = frame_0.clone();
+        cv::drawKeypoints(frame_0, keypoints, output);
         cv::imshow("test",output);
-        pressed_key = cv::waitKey(0);
-        
-        img1 = img2.clone();
-        
+        pressed_key = cv::waitKey(0); 
 
     }
     
