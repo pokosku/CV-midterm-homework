eda8e7408a3044590d7260d1a31ed3ac39b12f08
pokosku <fabbroleonardo@gmail.com>
2026-04-23 11:34:49 +0200

    prima implementazione del BFmatcher

---
+++ include/feature_matching.hpp
@@ -0,0 +1,11 @@
+#ifndef FEATURE_MATCHING_H
+#define FEATURE_MATCHING_H
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d.hpp>
+#include <iostream>
+
+std::vector<cv::Point2f> match_features(const cv::Mat& desc1,const std::vector<cv::KeyPoint> kp1, const cv::Mat& desc2, const std::vector<cv::KeyPoint> kp2);
+
+#endif
--- src/feature_matching.cpp
+++ src/feature_matching.cpp
@@ -1,26 +1,26 @@
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+#include "feature_matching.hpp"
 
-cv::Mat match_features(std::vector<cv::Keypoint> kp1, std::vector<cv::Keypoint> kp2){
+std::vector<cv::Point2f> match_features(const cv::Mat& desc1,const std::vector<cv::KeyPoint> kp1, const cv::Mat& desc2, const std::vector<cv::KeyPoint> kp2){
     cv::BFMatcher matcher(cv::NORM_L2);
 
     std::vector<std::vector<cv::DMatch>> knn_matches;
 
-    matcher.knnMatch(k1, k2, knn_matches, 2);
+    // descriptors are stored in Mat
+    matcher.knnMatch(desc1, desc2, knn_matches, 2);
     cv::Mat output;
     
-    //to be tester
+    //to be tested
     double ratio_thresh = 0.7;
     double movement_thresh = 5.0;
     
-    std::vector<cv::KeyPoint> points_on_object;
+    std::vector<cv::Point2f> points_on_object;
 
     for (size_t i = 0; i < knn_matches.size(); i++) {
     if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance) {
         
         
-        cv::Point2f p0 = k1[knn_matches[i][0].queryIdx].pt;
-        cv::Point2f pN = k2[knn_matches[i][0].trainIdx].pt;
+        cv::Point2f p0 = kp1[knn_matches[i][0].queryIdx].pt;
+        cv::Point2f pN = kp2[knn_matches[i][0].trainIdx].pt;
 
         float dist = cv::norm(p0 - pN);
 
@@ -31,7 +31,7 @@ cv::Mat match_features(std::vector<cv::Keypoint> kp1, std::vector<cv::Keypoint> 
 }
     //cv::drawKeypoints(moving_mask, keypoints, output);
     
-    return output;
+    return points_on_object;
 
 }
 
