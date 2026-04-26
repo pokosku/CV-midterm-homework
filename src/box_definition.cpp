cac613dbde4219c2bbc2de454a0fecbed30ec3f3
leo <fabbroleonardo@gmail.com>
2026-04-26 16:20:28 +0200

    commenti

--- src/box_definition.cpp
+++ src/box_definition.cpp
@@ -7,27 +7,30 @@
 
 
 cv::Rect bbox_from_clustering(const std::vector<cv::Point2f>& points, cv::Size frame_size) {
+
     if (points.empty()) return cv::Rect(0, 0, 0, 0);
 
-    // 1. Clustering spaziale
     std::vector<int> labels;
 
-    // Soglia di distanza: se due punti sono entro 40 pixel, sono "parenti"
+    // Distance threshold for neighbors, we choose 40 pixels
     float dist_threshold = 40.0f; 
     
+    // We split the points into clusters, following the condition of euclidean distance between two points and the threshold
     int n_clusters = cv::partition(points, labels, [dist_threshold](const cv::Point2f& a, const cv::Point2f& b) {
         return cv::norm(a - b) < dist_threshold;
     });
 
-    // 2. Trova il cluster più numeroso
+    // We compute the most crowded cluster
     std::vector<int> cluster_counts(n_clusters, 0);
     for (int label : labels) {
         cluster_counts[label]++;
     }
 
+
+    // Best cluster index
     int best_cluster_idx = std::distance(cluster_counts.begin(),  std::max_element(cluster_counts.begin(), cluster_counts.end()));
 
-    // 3. Estrai i punti appartenenti al miglior cluster
+    // Now we extract the points from the best cluster
     std::vector<cv::Point2f> best_cluster_points;
     for (size_t i = 0; i < points.size(); ++i) {
         if (labels[i] == best_cluster_idx) {
@@ -35,8 +38,10 @@ cv::Rect bbox_from_clustering(const std::vector<cv::Point2f>& points, cv::Size f
         }
     }
 
-    // 4. Calcola la Bounding Box
-    if (best_cluster_points.size() < 4) return cv::Rect(0, 0, 0, 0); // Evita cluster troppo piccoli
+    
+    if (best_cluster_points.size() < 4) return cv::Rect(0, 0, 0, 0);
+
+    // Output the bounding box of the clustered best points
     return cv::boundingRect(best_cluster_points);
 }
 
